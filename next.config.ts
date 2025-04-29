8e71f91f3a3a23e5b84b3f7197b1a867bbdca923
hilalozturk1 <hlloztrk68@gmail.com>
2025-04-29 23:18:20 +0300

    chakra config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      optimizePackageImports: ['@chakra-ui/react'],
+    },
+  }
+  
+  export default nextConfig;
+  
