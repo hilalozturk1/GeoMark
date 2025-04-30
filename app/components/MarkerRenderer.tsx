037382553cc06ad2c28e78502b85585441e30b34
hilalozturk1 <hlloztrk68@gmail.com>
2025-04-30 12:52:26 +0300

    marker

---
+++ app/components/MarkerRenderer.tsx
@@ -0,0 +1,29 @@
+import { Marker } from "@react-google-maps/api";
+import { Location } from "../types/location";
+
+type MarkerRendererProps = {
+  locations: Location[];
+  userLocation: { lat: number; lng: number } | null;
+};
+
+export default function MarkerRenderer({ locations, userLocation }: MarkerRendererProps) {
+  return (
+    <>
+      {userLocation && <Marker position={userLocation} label="You" />}
+      {locations.map((loc) => (
+        <Marker
+          key={loc.id}
+          position={{ lat: loc.latitude, lng: loc.longitude }}
+          label={loc.name}
+          icon={{
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 8,
+            fillColor: loc.color || "#FF0000",
+            fillOpacity: 1,
+            strokeWeight: 1,
+          }}
+        />
+      ))}
+    </>
+  );
+}
